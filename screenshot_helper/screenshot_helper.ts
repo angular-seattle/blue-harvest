099c845ba2aec019893028b65fe61a6face30644
Annie Wang <43010615+awang-copperleaf@users.noreply.github.com>
2018-10-02 18:26:49 -0700

    Improve compareScreenshot mask zIndex and output folder support (#12)
    - Updated README for screenshot support changes
    - Add diff output folder support
    - `addMask` now returns the mask object. The default z-index is set to 10000. Added `removeMask` function that takes in the mask object.
    - Added error-handling case for compareScreenShot()

--- screenshot_helper/screenshot_helper.ts
+++ screenshot_helper/screenshot_helper.ts
@@ -2,7 +2,8 @@ import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 
-import {browser, ElementFinder} from 'protractor';
+import { browser, ElementFinder, WebElement } from 'protractor';
+
 let mask_fn = require('./mask').MASK_FN;
 
 /*
@@ -24,51 +25,75 @@ let looksSame: LooksSame = require('looks-same');
  * variable is set, the promise resolves to true and the
  * golden image is updated.
  *
- * @param data The screenshot image data.
- * @param golden The path to the golden image to compare to.
+
+ * @param data - The screenshot image data.
+ * @param golden - The path to the golden image to compare to.
+ * @param outputFolder - The destination path for saving the diff. if it is not provided, the difference image will not be
+
+ *   saved.
  */
-export async function compareScreenshot(data, golden) {
-  let screenshotPath = await writeScreenshot(data);
-  const update = process.env['UPDATE_GOLDENS'] == "1"||
-      process.env['UPDATE_GOLDENS'] === "true";
-  if (update) {
-    console.log('Updating reference images instead of comparing.');
-    fs.writeFileSync(golden, fs.readFileSync(screenshotPath));
-    return true;
-  } else {
-    return new Promise<boolean>((resolve, reject) => {
-      looksSame(screenshotPath, golden, {strict: false, tolerance: 2.5}, (error, equal) => {
-        if (!equal) {
+export async function compareScreenshot(data, golden, outputFolder = undefined): Promise<string> {
+  return new Promise<string>(async (resolve, reject) => {
+    const tempFolder = createTempFolder();
+    const screenshotPath = await writeScreenshot(tempFolder, data);
+    // check if goldens need to be updated
+    const update = process.env['UPDATE_GOLDENS'] === '1' || process.env['UPDATE_GOLDENS'] === 'true';
+    if (update) {
+      fs.writeFileSync(golden, fs.readFileSync(screenshotPath));
+      resolve('Reference image ' + golden +' was successfully updated.');
+      return;
+    }
+    const goldenName = path.basename(golden);
+    looksSame(screenshotPath, golden, {
+      strict: false,
+      tolerance: 2.5,
+    }, async (error, equal) => {
+      if (error) {
+        reject("There has been an error. Error: " + error);
+        return;
+      }
+      if (!equal) {
+        if (outputFolder) {
+          const diffPath = `${outputFolder}/diff-${path.basename(golden)}`;
+          console.log(diffPath);
           looksSame.createDiff({
             reference: golden,
             current: screenshotPath,
-            diff: `diff-${path.basename(golden)}`,  // create a diff with the name of the golden file
+            diff: diffPath,
             highlightColor: '#ff00ff',  // color to highlight the differences
-          }, (error) => {
-            reject(`Screenshots do not match for ${golden}.`)
+          }, (err) => {
+            reject('SAVING DIFF ERROR: ' + err);
+            return;
           });
-        } else {
-          resolve(true);
-        }
-      });
+          reject(`Screenshots do not match for ${golden}. Difference picture is saved as ${diffPath}.`);
+        } else { reject(`Screenshots do not match for ${golden}.`); }
+      } else {
+        resolve('The test passed. ');
+      }
     });
-  }
+  });
 }
 
+function createTempFolder() {
+  return fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
+}
 /**
  *  Write a screenshot to disk in a new temporary path.
  */
-async function writeScreenshot(data) {
-  const folder = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
+async function writeScreenshot(folder, data) {
   let screenshotFile = path.join(folder, 'new.png');
   fs.writeFileSync(screenshotFile, data, 'base64');
   return screenshotFile;
 }
 
-export async function addMask(el: ElementFinder, color) {  
+export async function addMask(el: ElementFinder, color, zIndex = 10000, xOffset = 0, yOffset = 0, sizeMultiplier = 1.0) {
   let size = await el.getSize();
   let location = await el.getLocation();
-  await browser.executeScript(mask_fn,
-    location.x, location.y,
-    size.width, size.height, color);
+  const mask: WebElement = <WebElement> await browser.executeScript(mask_fn, location.x + xOffset, location.y + yOffset,
+    size.width*sizeMultiplier, size.height*sizeMultiplier, color, zIndex);
+  return mask;
+}
+
+export async function removeMask(mask: WebElement) {
+  await browser.executeScript("arguments[0].parentNode.removeChild(arguments[0])", mask);
 }
